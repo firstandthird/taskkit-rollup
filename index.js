5b7f9fe828ecc7285a0ed2e0df10bb900061dd0b
Greg Allen <greg@firstandthird.com>
2017-01-23 13:16:45 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+const TaskKitTask = require('taskkit-task');
+const rollup = require('rollup').rollup;
+const babel = require('rollup-plugin-babel');
+const nodeResolve = require('rollup-plugin-node-resolve');
+
+class RollupTask extends TaskKitTask {
+
+  get description() {
+    return 'Compiles your various client-executable files into a minified, source-mapped, browser-compatible js file that you can embed in a webpage';
+  }
+
+  process(input, filename, done) {
+    rollup({
+      entry: input,
+      plugins: [
+        babel({
+          exclude: 'node_modules/**'
+        }),
+        nodeResolve({
+          module: true,
+          main: true,
+          browser: true
+        })
+      ]
+    }).then(bundle => {
+      const result = bundle.generate({
+        //output format - 'amd', 'cjs', 'es', 'iife', 'umd'
+        format: 'iife',
+        moduleName: this.options.name,
+      });
+      this.write(filename, result.code, done);
+    }).catch(err => {
+      done(err);
+    });
+  }
+}
+module.exports = RollupTask;
