47784a6c3ab3583097a161488ffd4211223a32f2
Greg Allen <greg@firstandthird.com>
2017-01-24 11:25:26 -0800

    support for commonjs if passed in as option

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const TaskKitTask = require('taskkit-task');
 const rollup = require('rollup').rollup;
 const babel = require('rollup-plugin-babel');
 const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const builtins = require('rollup-plugin-node-builtins');
+const globals = require('rollup-plugin-node-globals');
 
 class RollupTask extends TaskKitTask {
 
@@ -18,16 +21,23 @@ class RollupTask extends TaskKitTask {
       babelPresets.push(['babili']);
     }
     const plugins = [
-      babel({
-        exclude: 'node_modules/**',
-        presets: babelPresets
-      }),
       nodeResolve({
         module: true,
         main: true,
         browser: true
       })
     ];
+    if (this.options.commonjs) {
+      plugins.push(globals());
+      plugins.push(builtins());
+      plugins.push(commonjs(this.options.commonjs));
+    }
+    plugins.push(babel({
+      exclude: 'node_modules/**',
+      presets: babelPresets,
+      babelrc: false
+    }));
+
     rollup({
       entry: input,
       plugins
