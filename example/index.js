eee58e2566aa3896c37247ef75661f07f16648e7
Greg Allen <greg@firstandthird.com>
2017-01-27 12:32:49 -0800

    more advanced example

--- example/index.js
+++ example/index.js
@@ -1,6 +1,16 @@
 import Domodule from 'domodule';
+import Complete from '@firstandthird/complete';
+import { ready } from 'domassist';
 import bestNumber from './commonjs';
 console.log(bestNumber);
+class Test {
+
+}
+const test = new Test();
 import { foo, bar } from './lib';
 const a = foo();
 console.log(a);
+ready(() => {
+  console.log('ready!');
+  console.log(Complete);
+});
